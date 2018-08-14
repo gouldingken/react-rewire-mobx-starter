be271edbc9441f86c7a9c28faa9ecc45a6291646
Ken Goulding <kgoulding@sasaki.com>
2018-08-14 14:55:16 -0400

    add support for publicPath

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # react-rewire-mobx-starter
 React +MobX boilerplate using https://github.com/timarney/react-app-rewired/tree/master/packages/react-app-rewire-mobx
 
+Also include `react-app-rewire-output` to support relative pathing as "publicPath" in order to deploy builds to non-root folders.
+
 Run `yarn install` to get setup.
--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
 const { injectBabelPlugin } = require("react-app-rewired");
 const rewireMobX = require("react-app-rewire-mobx");
+const rewireWebpackOutput = require('react-app-rewire-output');
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin("babel-plugin-styled-components", config);
     config = rewireMobX(config, env);
+
+    config = rewireWebpackOutput(config, env, {
+        publicPath: './',
+    });
 
     return config;
 };
